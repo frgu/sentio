e337ee091ac5fe9206a59bfcb939adbe6ac3416a
Freddy Gu <fgu@asymmetrik.com>
2016-07-19 13:01:49 -0400

    Initial placeholder for sankey

--- dist/sentio-angular.js
+++ dist/sentio-angular.js
@@ -168,7 +168,8 @@ function($document, $window, $timeout, $log) {
 			pointHoverFn: '&sentioPointHoverFn',
 			legendFn: '&sentioLegendFn',
 			yLock: '=sentioYLock',
-			stacked: '=sentioStacked'
+			stacked: '=sentioStacked',
+			showMarkers: '=sentioShowMarkers'
 		},
 		replace : false,
 		link : function(scope, element, attrs, controller) {
@@ -237,6 +238,11 @@ function($document, $window, $timeout, $log) {
 			scope.$watch('stacked', function(n, o) {
 				if (null === 0 && null == n) {return; }
 				line.stacked(n);
+			});
+
+			scope.$watch('showMarkers', function(n, o) {
+				if (null === 0 && null == n) {return; }
+				line.showMarkers(n);
 			});
 
 			scope.$watch('yLock', function(n, o) {
@@ -900,4 +906,39 @@ function($document, $window, $timeout, $log) {
 			});
 		}
 	};
+}]);
+
+angular.module('sentio').directive('sentioSankeyDiagram', ['$document', '$window', '$timeout', '$log',
+function($document, $window, $timeout, $log) {'use strict';
+
+	return {
+		restrict : 'A',
+		scope : {
+			model: '=sentioModel'
+		},
+		replace : false,
+		link : function(scope, element, attrs, controller) {
+			var sankeyElement = d3.select(element[0]);
+			var sankey = sentio.sankey.basic();
+
+			// Extract the height and width of the chart
+			var width = element[0].style.width;
+			if(null != width && '' !== width) { 
+				width = parseFloat(width.substring(0, width.length-2));
+				// if(null != width && !isNaN(width)) { line.width(width); }
+			}
+			var height = element[0].style.height;
+			if(null != height && '' !== height) {
+				height = parseFloat(height.substring(0, height.length-2));
+				// if(null != height && !isNaN(height)) { line.height(height); }
+			}
+			sankey.init(sankeyElement);
+
+			scope.$watchCollection('model', function(n, o){
+				if(null == o && null == n){ return; }
+				sankey.model(n);
+			});
+		}
+	};
+
 }]);
